968061bd2f6ac97e6dc596725110b90693c340e2
ignite720 <ignite720@outlook.com>
2025-02-15 21:50:52 +0800

    Added tableflatten

--- wkslight.lua
+++ wkslight.lua
@@ -11,14 +11,14 @@ m._VERSION = "0.0.1"
 --
 -- Local functions
 --
-local function s_table_print(v, indent)
+local function __table_print(v, indent)
     local variable_name = tostring(v)
     if type(v) == "table" then
         for k, v in pairs(v) do
             if type(v) == "table" then
                 print(indent .. "[" .. k .. "] => " .. variable_name .. " {")
                 
-                s_table_print(v, indent .. "    ")
+                __table_print(v, indent .. "    ")
             elseif type(v) == "string" then
                 print(indent .. "[" .. k .. '] => "' .. v  .. '"')
             else
@@ -30,7 +30,7 @@ local function s_table_print(v, indent)
     end
 end
 
-local function s_table_to_jsonish_array(tbl)
+local function __table_to_jsonish_array(tbl)
     return "'[\"" .. table.concat(tbl, "\", \"") .. "\"]'"
 end
 
@@ -70,8 +70,28 @@ function m.tableprint(tbl)
     assert(type(tbl) == "table")
     
     print(tostring(tbl) .. " {")
-    s_table_print(tbl, "  ")
+    __table_print(tbl, "  ")
     print("}\n")
+end
+
+function m.tableflatten(tbl)
+    assert(type(tbl) == "table")
+
+    local result = {}
+    local __tableflatten = function(t)
+        for k, v in pairs(t) do
+            if type(v) == "table" then
+                __tableflatten(v)
+            else
+                if k ~= "n" then
+                    table.insert(result, v)
+                end
+            end
+        end
+    end
+
+    __tableflatten(tbl)
+    return result
 end
 
 function m.tablemerge(tbl, tbl2)
@@ -81,7 +101,7 @@ function m.tablemerge(tbl, tbl2)
 end
 
 function m.table2jsonisharray(tbl)
-    return s_table_to_jsonish_array(tbl)
+    return __table_to_jsonish_array(tbl)
 end
 
 function m.bitmaskset(bmask, flag)
@@ -153,7 +173,7 @@ function m.wasmlinkoptions(opts)
     if m.bitmasktestany(opts.flags, m.EWasmFlag.USE_SDL_IMAGE) then
         linkoptions({
             "-sUSE_SDL_IMAGE=2",
-            "-sSDL2_IMAGE_FORMATS=" .. s_table_to_jsonish_array(opts.image_formats),
+            "-sSDL2_IMAGE_FORMATS=" .. __table_to_jsonish_array(opts.image_formats),
         })
     end
     if m.bitmasktestany(opts.flags, m.EWasmFlag.USE_SDL_MIXER) then
@@ -188,7 +208,7 @@ function m.wasmlinkoptions(opts)
         --linkoptions({ "-sEVAL_CTORS" })
     end
     if #opts.asyncify_whitelist > 0 then
-        linkoptions({ "-sASYNCIFY_WHITELIST=" .. s_table_to_jsonish_array(opts.asyncify_whitelist) })
+        linkoptions({ "-sASYNCIFY_WHITELIST=" .. __table_to_jsonish_array(opts.asyncify_whitelist) })
     end
     
     if m.bitmasktestany(opts.flags, m.EWasmFlag.LINK_OPENAL) then
@@ -198,10 +218,10 @@ function m.wasmlinkoptions(opts)
     end
     
     if #opts.exported_runtime_methods > 0 then
-        linkoptions({ "-sEXPORTED_RUNTIME_METHODS=" .. s_table_to_jsonish_array(opts.exported_runtime_methods) })
+        linkoptions({ "-sEXPORTED_RUNTIME_METHODS=" .. __table_to_jsonish_array(opts.exported_runtime_methods) })
     end
     if #opts.exported_functions > 0 then
-        linkoptions({ "-sEXPORTED_FUNCTIONS=" .. s_table_to_jsonish_array(opts.exported_functions) })
+        linkoptions({ "-sEXPORTED_FUNCTIONS=" .. __table_to_jsonish_array(opts.exported_functions) })
     end
     
     for i, v in ipairs(opts.preload_files) do
